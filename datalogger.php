1bc27daad17f18e49ed77f1e382901e65eba52e9
Nicholas Grant <nicholas.grant1@maine.edu>
2014-04-30 22:09:37 +0000

    Graphing temperature, but no axes on graph and its not pretty yet

--- datalogger.php
+++ datalogger.php
@@ -4,50 +4,72 @@
 
 #var_dump(gd_info());
 
-/*
+
 Header( "Content-type: image/gif");
 
-$image = imagecreate(200,200);
-$maroon = imagecolorallocate($image, 100, 0, 0);
+$image = imagecreate(400,400);
+
+$red = imagecolorallocate($image, 255, 0, 0);
 $white = imagecolorallocate($image, 255, 255, 255);
 $blue = imagecolorallocate($image, 0, 0, 255);
 $grey = imagecolorallocate($image, 200, 200, 200);
 
+
 #Draw a rectangle
-ImageFilledRectangle($image, 0, 0, 200, 200, $grey); 
+ImageFilledRectangle($image, 0, 0, 400, 400, $grey); 
 
-#Output image
-imagegif($image);
-
-#Destroy image being stored in memory
-imagedestroy($image);
-*/
 
 #Pull data from database
 $database = new SQLite3('/home/pi/ece331/project2/templog.db');
-$results = $database->query('SELECT * FROM temps');
+$results = $database->query('SELECT temp FROM temps');
+$timestamps = $database->query('SELECT timestamp FROM temps');
 
 #Stores the number of rows of temperature data in 'num_rows'
 $rows = $database->query("SELECT COUNT(*) as count FROM temps");
 $row1 = $rows->fetchArray();
 $num_rows = $row1['count'];
 
-var_dump($num_rows);
+#var_dump($num_rows);
 
-#One by one prints out 
-while($row = $results->fetchArray()) {
-	var_dump($row);
+$xincrement = bcdiv(400, $num_rows-1,0);
+$x=0;
+$i=0;
+
+#var_dump($xincrement);
+
+//Loop while we still have rows of data
+while($dataRow= $results->fetchArray()) {
+	
+	//Calculate y-coordinate
+	$y = $dataRow[0];
+	
+#	var_dump($x);
+#	var_dump($y);
+
+	//Add values into points array
+	$points[$i][0] = $x;
+	$points[$i][1] = $y;
+	
+#	var_dump(gettype($points[$i][0]));
+#	var_dump(gettype($points[$i][1]));
+
+	//Increment x by xincrement
+	$x+=$xincrement;
+	$i++;
+}
+
+for($i=0;$i<$num_rows-1;$i++){
+	#var_dump($points[$i][0]);
+	#var_dump($points[$i][1]);
+	$error = ImageLine($image,$points[$i][0],400-$points[$i][1],$points[$i+1][0],400-$points[$i+1][1],$red);
+#	imageline($image, 0, 0, 200, 200, $red);
 }
 
 
-/*
-header("Content-Type: image/png");
-$im = @imagecreate(110, 20)
-    or die("Cannot Initialize new GD image stream");
-$background_color = imagecolorallocate($im, 0, 0, 0);
-$text_color = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5,  "A Simple Text String", $text_color);
-imagepng($im);
-imagedestroy($im);
-*/
+//Output GIF and free memory
+imagegif($image);
+
+#Destroy image being stored in memory
+imagedestroy($image);
+
 ?>
