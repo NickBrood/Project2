e511502f4b2b0dbf282ca5ee18c20fe789ca09e8
Nicholas Grant <nicholas.grant1@maine.edu>
2014-05-01 01:27:07 +0000

    More thorough commenting, working on pretty axes for graph

--- datalogger.php
+++ datalogger.php
@@ -4,10 +4,10 @@
 
 #var_dump(gd_info());
 
-
+/*
 Header( "Content-type: image/gif");
 
-$image = imagecreate(400,400);
+$image = imagecreate(720,720);
 
 $red = imagecolorallocate($image, 255, 0, 0);
 $white = imagecolorallocate($image, 255, 255, 255);
@@ -16,8 +16,8 @@ $grey = imagecolorallocate($image, 200, 200, 200);
 
 
 #Draw a rectangle
-ImageFilledRectangle($image, 0, 0, 400, 400, $grey); 
-
+ImageFilledRectangle($image, 0, 0, 720, 720, $grey); 
+*/
 
 #Pull data from database
 $database = new SQLite3('/home/pi/ece331/project2/templog.db');
@@ -29,9 +29,23 @@ $rows = $database->query("SELECT COUNT(*) as count FROM temps");
 $row1 = $rows->fetchArray();
 $num_rows = $row1['count'];
 
+#Parse Timestamp for useful numbers
+$minutes = $database->query('SELECT strftime("%M", time(timestamp)) FROM temps');
+$hours = $database->query('SELECT strftime("%H", time(timestamp)) FROM temps');
+$day = $database->query('SELECT strftime("%d", time(timestamp)) FROM temps');
+$dayofweek = $database->query('SELECT strftime("%w", time(timestamp)) FROM temps');
+
+$minutes = intval($minutes);
+
+var_dump($minutes);
+var_dump($hours);
+var_dump($day);
+var_dump($dayofweek);
+
+
 #var_dump($num_rows);
 
-$xincrement = bcdiv(400, $num_rows-1,0);
+$xincrement = bcdiv(720, $num_rows-1,0);
 $x=0;
 $i=0;
 
@@ -61,15 +75,15 @@ while($dataRow= $results->fetchArray()) {
 for($i=0;$i<$num_rows-1;$i++){
 	#var_dump($points[$i][0]);
 	#var_dump($points[$i][1]);
-	$error = ImageLine($image,$points[$i][0],400-$points[$i][1],$points[$i+1][0],400-$points[$i+1][1],$red);
+#	$error = ImageLine($image,$points[$i][0],720-$points[$i][1],$points[$i+1][0],720-$points[$i+1][1],$red);
 #	imageline($image, 0, 0, 200, 200, $red);
 }
 
-
+/*
 //Output GIF and free memory
 imagegif($image);
 
 #Destroy image being stored in memory
 imagedestroy($image);
-
+*/
 ?>
